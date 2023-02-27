2d508aa90d87e19e4bcc9c000081a8d46ee3dac9
Ashish Ranjan <67757899+AshishRanjan2910@users.noreply.github.com>
2023-02-27 15:57:41 +0530

    ExpenseFilter work based on year initiated

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,11 +1,21 @@
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
-import "./Expenses.css"
+import ExpensesFilter from "./ExpensesFilter";
+import "./Expenses.css";
 
 export default function ExpenseAligner(props) {
   const expenses = props.expenses;
+  const [filteredYear, setFilteredYear] = useState("2019")
+
+  const yearSelectHandler = (selectedYear) => {
+    console.log(selectedYear);
+    setFilteredYear(selectedYear);
+  };
+
   return (
     <Card className="expenses">
+      <ExpensesFilter yearSelected={filteredYear} onYearSelect={yearSelectHandler} />
       <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}
@@ -27,5 +37,5 @@ export default function ExpenseAligner(props) {
         date={expenses[3].date}
       />
     </Card>
-  )
+  );
 }
--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -1,13 +1,14 @@
-import React from 'react';
-
 import './ExpensesFilter.css';
 
-const ExpensesFilter = () => {
+const ExpensesFilter = (props) => {
+  const selectHandler = (event) => {
+    props.onYearSelect(event.target.value)
+  }
   return (
     <div className='expenses-filter'>
       <div className='expenses-filter__control'>
         <label>Filter by year</label>
-        <select>
+        <select onChange={selectHandler} value={props.yearSelected}>
           <option value='2022'>2022</option>
           <option value='2021'>2021</option>
           <option value='2020'>2020</option>
